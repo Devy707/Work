47688668eb98a7de1e7585dea68c55de05b15e17
Bogdan <devy.02@ukr.net>
2021-01-04 12:54:23 +0200

    first practice work

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-let a = 10;
-a = 11;
-console.log(a);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+let personMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+const firstQuestion = prompt('Один из последних просмотренных фильмов?',''),
+      secondQuestion = prompt('На сколько оцените его?',''),
+      thirdQuestion = prompt('Один из последних просмотренных фильмов?',''),
+      fourthQuestion = prompt('На сколько оцените его?','');
+personMovieDB.movies[firstQuestion] = secondQuestion;
+personMovieDB.movies[thirdQuestion] = fourthQuestion;
+console.log(personMovieDB);
